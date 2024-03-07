41f7fce916ec8d0665abfc8b18b2a0ff55127e5f
CRIS-LI-DEV <131208714+CRIS-LI-DEV@users.noreply.github.com>
2024-03-06 21:32:34 -0300

    Update Controller.java

--- src/main/java/com/company/microservice/controller/Controller.java
+++ src/main/java/com/company/microservice/controller/Controller.java
@@ -7,6 +7,74 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+public class Animal {
+    private String nombre;
+    private String habitat;
+    private String dieta;
+    private int longevidad;
+    private int promedioVida;
+    private String estado;
+
+    // Constructor
+    public Animal(String nombre, String habitat, String dieta, int longevidad, int promedioVida, String estado) {
+        this.nombre = nombre;
+        this.habitat = habitat;
+        this.dieta = dieta;
+        this.longevidad = longevidad;
+        this.promedioVida = promedioVida;
+        this.estado = estado;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getDieta() {
+        return dieta;
+    }
+
+    public void setDieta(String dieta) {
+        this.dieta = dieta;
+    }
+
+    public int getLongevidad() {
+        return longevidad;
+    }
+
+    public void setLongevidad(int longevidad) {
+        this.longevidad = longevidad;
+    }
+
+    public int getPromedioVida() {
+        return promedioVida;
+    }
+
+    public void setPromedioVida(int promedioVida) {
+        this.promedioVida = promedioVida;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
+
 @RestController
 public class Controller {
 
